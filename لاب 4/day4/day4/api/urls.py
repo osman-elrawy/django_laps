a339a26b2ad96409ace405cac7cd45ca287a17fc
osman-elrawy <82684751+osman-elrawy@users.noreply.github.com>
2022-06-25 02:43:24 +0200

    لاب 4
    c

--- لاب 4/day4/day4/api/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from .views import *
-urlpatterns = [
-    path('/list',getData),
-    path('/insert',inserttry),
-    path('/delete/<pk>',delete),
-    path('/update/<pk>',update)
-]
